bc117b2f406148d2642893283aedf4f111c5fffe
Elly <145195760+ellypaws@users.noreply.github.com>
2024-04-30 13:50:07 +0800

    Commit extension api

---
+++ go.mod
@@ -0,0 +1,50 @@
+module github.com/ellypaws/inkbunny-app/extension
+
+go 1.22.2
+
+require (
+	github.com/ellypaws/inkbunny-sd v0.0.0-20240430045340-8ae4823b535e
+	github.com/labstack/echo/v4 v4.12.0
+	github.com/labstack/gommon v0.4.2
+	github.com/muesli/termenv v0.15.2
+)
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coocood/freecache v1.2.4 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/ellypaws/inkbunny-app v0.0.0-20240430045830-f791ca57eb08
+	github.com/ellypaws/inkbunny/api v0.0.0-20240411110242-d491ced97f23 // indirect
+	github.com/gitsight/go-echo-cache v1.0.1 // indirect
+	github.com/go-errors/errors v1.5.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mcuadros/go-defaults v1.2.0 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/redis/go-redis/v9 v9.5.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	modernc.org/gc/v3 v3.0.0-20240304020402-f0dba7c97c2b // indirect
+	modernc.org/libc v1.50.4 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/sqlite v1.29.8 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	_ "embed"
+	"encoding/json"
+	"github.com/ellypaws/inkbunny-app/cmd/api"
+	"github.com/ellypaws/inkbunny-app/cmd/db"
+	sd "github.com/ellypaws/inkbunny-sd/stable_diffusion"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	logger "github.com/labstack/gommon/log"
+	"github.com/muesli/termenv"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	sdHost    = sd.DefaultHost   // SD_HOST
+	port      = "1323"           // PORT
+	redisHost = "localhost:6379" // REDIS_HOST
+
+	e = echo.New()
+)
+
+func main() {
+	e.Use(middleware.Recover())
+
+	for _, m := range middlewares {
+		e.Use(m)
+	}
+
+	config := append(api.WithRedis,
+		[]echo.MiddlewareFunc{
+			api.SIDMiddleware,
+			api.Anonymous,
+		}...)
+
+	e.GET("/review/:id", api.GetReviewHandler, config...)
+	e.POST("/review/:id", api.GetReviewHandler, config...)
+
+	e.Logger.Infof("Starting server on port %s", port)
+	e.Logger.Fatal(e.Start(":" + port))
+}
+
+//go:embed artists.json
+var artistsJSON []byte
+
+//go:embed auditors.json
+var auditorsJSON []byte
+
+var middlewares = []echo.MiddlewareFunc{
+	middleware.LoggerWithConfig(
+		middleware.LoggerConfig{
+			Skipper:          nil,
+			Format:           `${time_custom}     	${status} ${method}  ${host}${uri} in ${latency_human} from ${remote_ip} ${error}` + "\n",
+			CustomTimeFormat: time.DateTime,
+		},
+	),
+	middleware.RemoveTrailingSlash(),
+	middleware.Gzip(),
+	middleware.Decompress(),
+	middleware.NonWWWRedirect(),
+	middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}),
+}
+
+func init() {
+	e.Logger.SetLevel(logger.DEBUG)
+	e.Logger.SetHeader(`${time_rfc3339} ${level}	${short_file}:${line}	`)
+
+	if h := os.Getenv("SD_HOST"); h != "" {
+		u, err := url.Parse(h)
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+		sdHost = (*sd.Host)(u)
+	} else {
+		e.Logger.Warn("warning: SD_HOST not set, using default localhost:7860")
+	}
+
+	if p := os.Getenv("PORT"); p != "" {
+		port = p
+	} else {
+		e.Logger.Warn("env PORT not set, using default 1323")
+	}
+
+	envApiHost := os.Getenv("API_HOST")
+	if envApiHost == "" {
+		e.Logger.Warnf("env API_HOST is not set, using default localhost:%s\n", port)
+		api.ServerHost = &url.URL{
+			Scheme: "http",
+			Host:   "localhost:" + port,
+		}
+	} else {
+		apiHost, err := url.Parse(envApiHost)
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+		api.ServerHost = apiHost
+	}
+
+	var err error
+	api.Database, err = db.New(nil)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	var artists []db.Artist
+	err = json.Unmarshal(artistsJSON, &artists)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	err = api.Database.UpsertArtist(artists...)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	var auditors []db.Auditor
+	err = json.Unmarshal(auditorsJSON, &auditors)
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	for i := range auditors {
+		err = api.Database.InsertAuditor(auditors[i])
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+	}
+
+	e.HideBanner = true
+
+	colors := []struct {
+		text  string
+		color string
+	}{
+		{"M", "#447294"},
+		{"a", "#4f7d9e"},
+		{"i", "#5987a8"},
+		{"n", "#6492b2"},
+		{"t", "#6f9cbd"},
+		{"a", "#7aa7c7"},
+		{"i", "#84b1d1"},
+		{"n", "#8fbcdb"},
+		{"e", "#a0c0d6"},
+		{"d", "#b1c5d1"},
+		{" ", "#c2c9cc"},
+		{"b", "#d2cdc6"},
+		{"y", "#e3d2c1"},
+		{":", "#f4d6bc"},
+	}
+
+	var coloredText strings.Builder
+	for _, ansi := range colors {
+		coloredText.WriteString(termenv.String(ansi.text).Foreground(termenv.RGBColor(ansi.color)).Bold().String())
+	}
+
+	e.Logger.Infof("%s %s", coloredText.String(), "https://github.com/ellypaws")
+	e.Logger.Infof("Post issues at %s", "https://github.com/ellypaws/inkbunny-extension/issues")
+
+	e.Logger.Infof("     api host: %s", api.ServerHost)
+	if sdHost.Alive() {
+		e.Logger.Infof("      sd host: %s", api.SDHost)
+	} else {
+		e.Logger.Warnf("      sd host: %s (not running)", api.SDHost)
+	}
+}
